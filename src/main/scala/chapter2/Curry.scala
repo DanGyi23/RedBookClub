4a18481d2ecaf42d6b70b15ab3cbf02693db57d1
Dan Gyi <Dan>
2020-02-25 11:34:04 +0000

    curry time

---
+++ src/main/scala/chapter2/Curry.scala
@@ -0,0 +1,18 @@
+package chapter2
+
+object Curry {
+  def curry[A, B, C](f: (A, B) => C): A => B => C =
+    x => y => f(x, y)
+}
+
+object Uncurry {
+  def uncurry[A, B, C](f: A => (B => C)): (A, B) => C = {
+    (x, y) => f(x)(y)
+  }
+}
+
+object Compose {
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    x => f(g(x))
+  }
+}
