82cf15018c851f023c58dde989e7a76d5bd273d2
Dan Gyi <Dan>
2020-03-03 18:43:27 +0000

    exercise 3.11 done

--- src/main/scala/chapter3/Beans.scala
+++ src/main/scala/chapter3/Beans.scala
@@ -15,6 +15,11 @@ object Beans {
     case Cons(x, xs) => x + sum(xs)
   }
 
+  def product(ints: Beans[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x * product(xs)
+  }
+
   def apply[A](as: A*): Beans[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
@@ -33,15 +38,61 @@ object Beans {
     }
   }
 
+  def setHead[A](as: Beans[A], newHead: A): Beans[A] = {
+    Cons(newHead, tail(as).getOrElse(Nil))
+  }
+
   def drop[A](list: Beans[A], n: Int): Beans[A] = {
     def loop(z: Int, modTail: Beans[A]): Beans[A] = {
       if (z == 0) modTail
       else loop(z - 1, tail(modTail).getOrElse(Nil))
     }
+
     loop(n, list)
   }
 
+  def dropWhile[A](list: Beans[A])(f: A => Boolean): Beans[A] = {
+    list match {
+      case Cons(h, t) if f(h) => dropWhile(t)(f)
+      case _ => list
+    }
+  }
 
+  def foldRight[A, B](list: Beans[A], z: B)(f: (A, B) => B): B = {
+    list match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+  }
+
+  def length[A](list: Beans[A]): Int = {
+    foldRight(list, 0)((_, xs) => xs + 1)
+  }
+
+  def foldLeft[A, B](list: Beans[A], z: B)(f: (B, A) => B): B = {
+    list match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
+  }
+
+  def sumFl(list: Beans[Int]): Int = {
+    foldLeft(list, 0)((y,z) => y + z)
+  }
+
+  def productFl(list: Beans[Int]): Int = {
+    foldLeft(list, 1)((y,z) => y * z)
+  }
+
+  def lengthFl[A](list: Beans[A]): Int = {
+    foldLeft(list, 0)((z, _) => z + 1)
+  }
+
+  //  def dropLast[A](list: Beans[A]): Beans[A] = {
+  //    def loop(list: Beans[A]) = {
+  //
+  //    }
+  //  }
 
 
   val x = Beans(1, 2, 3, 4, 5) match {
--- src/test/scala/chapter3/BeansSpec.scala
+++ src/test/scala/chapter3/BeansSpec.scala
@@ -18,6 +18,10 @@ class BeansSpec extends AnyFreeSpec with Matchers {
       Beans.head(Beans()) shouldBe None
     }
 
+    "setHead should change the first value in a list" in {
+      Beans.setHead(Beans(5,6,7,8,9), 69) shouldBe Beans(69,6,7,8,9)
+    }
+
 
     "tail(1,2,3) should be (2,3)" in {
       Beans.tail(Beans(1, 2, 3)) shouldBe Some(Beans(2, 3))
@@ -29,6 +33,33 @@ class BeansSpec extends AnyFreeSpec with Matchers {
 
     "drop (Beans(1,2,3,4,5), 2) should be Beans(3,4,5)" in {
       Beans.drop(Beans(1,2,3,4,5), 2) shouldBe Beans(3,4,5)
+    }
+
+    "dropWhile (Beans(10,2,3,4,5), (x: Int) => x % 5 == 0) should be Beans(2,3,4,5)" in {
+      Beans.dropWhile(Beans(10,2,3,4,5))((x: Int) => x % 5 == 0) shouldBe Beans(2,3,4,5)
+    }
+
+    "length Beans(_*) should be appropriate length" in {
+      Beans.length(Beans(10,24,35,41,52)) shouldBe 5
+      Beans.length(Beans("dogs", "cats", "bananas")) shouldBe 3
+    }
+
+    "foldLeft(1,2,3,4)(x,y => x + y) should be 10" in {
+      Beans.foldLeft(Beans(1,2,3,4), 0)((x, y) => x + y) shouldBe 10
+    }
+
+    "sumFl should be sum of Beans" in {
+      val list = Beans(1,2,3,4)
+      Beans.sumFl(list) shouldBe 10
+    }
+
+    "productFl should be product of Beans" in {
+      val list = Beans(1,2,3,4)
+      Beans.productFl(list) shouldBe 24
+    }
+
+    "lengthFl should return appropriate length for Beans" in {
+      Beans.lengthFl(Beans(1,2,3,4,5,6,7)) shouldBe 7
     }
   }
 }
